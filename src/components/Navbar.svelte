57c0291ce1699961f75db84b0e16acc32dc6a8a8
Rafida CM <138202592+rafida22@users.noreply.github.com>
2025-06-27 21:54:13 +0530

    Initial commit of Astro + Svelte + Tailwind project

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+
+<nav class="w-full shadow-md mb-3 bg-white text-gray-800 ">
+  <div class="max-w-screen-2xl py-5 mx-auto px-3 flex items-center justify-between">
+    <div class="font-bold text-xl text-black">LeapX</div>
+    <div class="flex space-x-15 font-medium  text-lg">
+      <a href="#" class="text-violet-500 ">Startups</a>
+      <a href="#" class="hover:text-violet-500 ">Documents</a>
+      <a href="#" class="hover:text-violet-500 ">Action Plan</a>
+      <a href="#" class="hover:text-violet-500 ">Mentorship Comments</a>
+      <a href="#" class="hover:text-violet-500 ">Internal Notes</a>
+      <a href="#" class="hover:text-violet-500 ">Meetings</a>
+      <a href="#" class="hover:text-violet-500 ">Reports</a>
+    </div>
+    <div>
+      <a href="/" class="text-lg hover:text-red-500">Logout</a>
+    </div>
+  </div>
+</nav>
+
+
+
+
